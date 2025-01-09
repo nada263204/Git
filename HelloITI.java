5e79a5e99d5c4cd729aa0d88a2664c292aac8508
nada gharib <106126824+nada263204@users.noreply.github.com>
2025-01-09 09:15:48 +0200

    first commit

---
+++ HelloITI.java
@@ -0,0 +1,1 @@
+﻿public class HelloITI { public void printHelloITI() { System.out.println('Hello ITI'); } }
---
+++ HelloJava.java
@@ -0,0 +1,1 @@
+﻿public class HelloWorld { public void printHelloWorld() { System.out.println('Hello World'); } }
